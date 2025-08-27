ac42b810c97826a8a9e0beb66f6a99434ad9f597
popalmatrix <popalmatrix@gmail.com>
2025-08-27 06:05:25 -0400

    Create print_a_single_line.py

---
+++ print_a_single_line.py
@@ -0,0 +1,16 @@
+# Fix the code
+# Fix the code into a single line
+#print(5)
+#print(" + ")
+#print(8)
+#print(" - ")
+#print(4)
+#print(" = ")
+#print(5 + 8 - 4)
+#
+
+x = 5
+y = 8
+z = 4
+result = x + y - z
+print(f"{x} + {y} - {z} = {result}")
