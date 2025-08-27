11395ad157bcd484e2af9e90dab962e58cf6476b
popalmatrix <popalmatrix@gmail.com>
2025-08-27 05:53:34 -0400

    Create name_and_address.py

---
+++ name_and_address.py
@@ -0,0 +1,9 @@
+# Write your solution here
+givenname = input("what is your given name")
+familyname = input("what is your family name")
+streetaddress = input("what is your street address")
+cityzip = input("what is your city and postal code?")
+print(givenname + " " + familyname)
+print(streetaddress)
+print(cityzip)
+ 
