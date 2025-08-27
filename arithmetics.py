82a69b4dd34baa7f485020a140bd3fbfb461f3a2
popalmatrix <popalmatrix@gmail.com>
2025-08-27 06:02:22 -0400

    Create arithmetics.py

---
+++ arithmetics.py
@@ -0,0 +1,16 @@
+# x = 27
+# y = 15
+# print(x, '+', y, '=', x + y)
+#print(x, '-', y, '=', x - y)
+# print(x, '*', y, '=', x * y)
+# print(x, '/', y, '=', x / y)
+#above and under are two ways to format a string for output. The first way is using f-strings (formatted string literals) with {} curly braces and the second is using the comma-separated print() function, as you seen above
+#n short, {} is for storing data in a key:value format, and '' single quotes is for storing a sequence of characters.
+ 
+x = 27
+y = 15
+print(f"{x} + {y} = {x + y}")
+print(f"{x} - {y} = {x - y}")
+print(f"{x} * {y} = {x * y}")
+print(f"{x} / {y} = {x / y}")
+ 
