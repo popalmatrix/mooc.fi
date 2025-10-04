6f93272ef0b7cef6938630f8c6462dbf367a4321
popalmatrix <popalmatrix@gmail.com>
2025-10-04 09:39:45 -0400

    Create line_of_hashes.py

---
+++ part_3_loops_conditions/working_with_strings/line_of_hashes.py
@@ -0,0 +1,9 @@
+# 1. Ask the user for the desired width (an integer)
+width = int(input("Width: "))
+
+# 2. Define the character we want to print
+lineofhashes = "#"
+
+# 3. Use the multiplication operator (*) to repeat the character
+#    The result is a string of '#' repeated 'width' number of times.
+print(lineofhashes * width)
